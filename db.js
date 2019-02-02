3ac586c6ae3dfdbf65bd3e76e4209d0159ecbc5f
USER <user@USERui-MacBookPro.local>
2019-02-02 17:37:16 +0900

    conn : mongodb

--- db.js
+++ db.js
@@ -1,51 +1,15 @@
-export const videos = [
-    {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is test Videos",
-        views: 24,
-        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-        creator: {
-            id: 121212,
-            name: "jeong",
-            email: "jeong@geon.com"
-        }
-    },
-    {
-        id: 123455,
-        title: "Video nice",
-        description: "This is test Videos",
-        views: 24,
-        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-        creator: {
-            id: 121212,
-            name: "jeong",
-            email: "jeong@geon.com"
-        }
-    },
-    {
-        id: 625353,
-        title: "Video perfect",
-        description: "This is test Videos",
-        views: 24,
-        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-        creator: {
-            id: 121212,
-            name: "jeong",
-            email: "jeong@geon.com"
-        }
-    },
-    {
-        id: 625353,
-        title: "Video beauty",
-        description: "This is test Videos",
-        views: 24,
-        videoFile: "http://techslides.com/demos/sample-videos/small.mp4",
-        creator: {
-            id: 121212,
-            name: "jeong",
-            email: "jeong@geon.com"
-        }
-    }
+import mongoose from "mongoose"
 
-]
+mongoose.connect("mongodb://localhost:27017/we-tube",
+{
+    useNewUrlParser:true,
+    useFindAndModify:false
+}
+)
+
+const db = mongoose.connection
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = err => console.log(`❌ Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen)
+db.on("error", handleError)
