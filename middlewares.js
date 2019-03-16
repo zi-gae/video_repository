c26af2ca9f6d381b77830c54b574c6ad61bc084b
doscmDev@gmail.com <doscmDev@gmail.com>
2019-03-16 18:03:33 +0900

    Refac: Edit profile
    프로필 사진 변경 기능

--- middlewares.js
+++ middlewares.js
@@ -2,7 +2,7 @@ import routes from "./routes";
 import multer from "multer";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
-
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "VideoPlayer";
   res.locals.routes = routes;
@@ -28,6 +28,7 @@ export const onlyPrivate = (req, res, next) => {
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
 // single() => 하나의 파일만 업로드 할 수 있다는 함수
 // 사용자가 전송한 데이터에서 파일이 포함 되어 있다면
 // 파일을 가공해서 req.file 이라는 프로퍼티를 암시적으로 추가하는 미들웨어
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,15 +3,17 @@ import routes from "../routes";
 import {
   users,
   userDetail,
-  editProfile,
-  changePassword
+  getEditProfile,
+  changePassword,
+  postEditProfile
 } from "../controllers/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 
 // userRouter.get(routes.users, users)
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
