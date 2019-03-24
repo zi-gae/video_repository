6e4f1ab26cbaf9aa8af23f28e4361d77e15d2f6b
devGW <doscmDev@gmail.com>
2019-03-24 23:59:56 +0900

    Add: player currentTime & totalTime 추가

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const voluemBtn = document.getElementById("jsVolumeButton");
 const screenBtn = document.getElementById("jsScreenButton");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 const toggleIcon = (playTF, muteTF) => {
   const playIcon = document.getElementById("jsPlayIcon");
@@ -29,11 +31,38 @@ const handleVolumeClick = () => {
 const goScreenClick = () => {
   videoPlayer.requestFullscreen();
 };
+const formatDate = seconds => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
 
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTime() {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+}
 const init = () => {
   playBtn.addEventListener("click", handlePlayClick);
   voluemBtn.addEventListener("click", handleVolumeClick);
   screenBtn.addEventListener("click", goScreenClick);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
 };
 
 if (videoContainer) {
