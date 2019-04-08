172eb895acb61255959b4097cfadadf8b5574c6c
devGW <doscmDev@gmail.com>
2019-04-08 20:23:57 +0900

    Edit: env

--- src/db.js
+++ src/db.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 
 dotenv.config(); //env파일의 variable 들을 process.env.key 로 저장
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
