2ba9961a6492435585982e8237c51ee3b3ed1c27
devGW <doscmDev@gmail.com>
2019-04-17 04:21:46 +0900

    REFACTOR

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -181,10 +181,10 @@ export const postAuthApply = async (req, res) => {
     body: { email }
   } = req;
   try {
-    const user = await User.findOne({ email });
-    user.authApply
-      ? await User.updateOne({ email }, { $set: { authApply: false } })
-      : await User.updateOne({ email }, { $set: { authApply: true } });
+    // const user = await User.findOne({ email });
+    // user.authApply
+    //   ? await User.updateOne({ email }, { $set: { authApply: false } })
+    //   : await User.updateOne({ email }, { $set: { authApply: true } });
   } catch (error) {
     console.log(error);
     res.redirect(routes.home);
